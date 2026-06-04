96f74e2f9288f75b721a9efe577c86b07292827b
ariniamalia <arini.amalia3112@gmail.com>
2026-06-04 11:35:37 +0700

    host

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "guestbook-bay-tau.vercel.app",
   user: "root",
   password: "", // default XAMPP kosong
   database: "guestbook"
